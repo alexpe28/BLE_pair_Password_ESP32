cf8e06bde1ebb205359a1bdfae22bc83029e710f
alexpe28 <yalaevalexey@gmail.com>
2024-06-06 15:51:47 +0300

    адреса не совпадают на одном устройстве. нужно исправить

--- src/main.cpp
+++ src/main.cpp
@@ -1,175 +1,185 @@
 #include <Arduino.h>
 #include <BLEDevice.h>
-#include <BLEServer.h>
 #include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLESecurity.h>
+#include <Preferences.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define PASSKEY 999999
+#define SERVICE_UUID "00001800-0000-1000-8000-00805f9b34fb"
+#define CHARACTERISTIC_UUID "00002a00-0000-1000-8000-00805f9b34fb"
+#define DEVICE_NAME "ESP32_BLE_DEVICE"
 
-SemaphoreHandle_t xGuiSemaphore;
+BLEServer* pServer = nullptr;
+BLECharacteristic* pCharacteristic = nullptr;
+bool deviceConnected = false;
+bool authenticated = false;
+uint8_t txValue = 0;
 
-bool ConStatus;
+const int ledPin = 2;
+Preferences preferences;
 
-/////////////////////
-// BLE Secure Server//
-/////////////////////
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t* param) {
+      deviceConnected = true;
+      uint8_t* addr = param->connect.remote_bda;
 
-class ServerCallback : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
-    Serial.println(" - ServerCallback - onConnect");
-  };
+      // Вывод адреса подключающегося устройства
+      Serial.print("Connected device address: ");
+      for (int i = 0; i < 6; i++) {
+        if (i != 0) {
+          Serial.print(":");
+        }
+        Serial.print(addr[i], HEX);
+      }
+      Serial.println();
 
-  void onDisconnect(BLEServer *pServer)
-  {
-    Serial.println(" - ServerCallback - onDisconnect");
-    ConStatus = false;
+      preferences.begin("ble_whitelist", true);
+      uint8_t peer_addr[6];
+      if (preferences.getBytes("peer_addr", peer_addr, sizeof(peer_addr)) == sizeof(peer_addr)) {
+        if (memcmp(addr, peer_addr, sizeof(peer_addr)) == 0) {
+          authenticated = true;
+          Serial.println("Device reconnected from whitelist");
+        } else {
+          authenticated = false;
+          Serial.println("Connected device not in whitelist");
+        }
+      } else {
+        authenticated = false;
+        Serial.println("No devices in whitelist");
+      }
+      preferences.end();
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      authenticated = false;
+      Serial.println("Device disconnected");
+      pServer->getAdvertising()->start();
+    }
+};
+
+class MySecurity : public BLESecurityCallbacks {
+  uint32_t onPassKeyRequest() {
+    Serial.println("PassKeyRequest");
+    return 123456;
+  }
+  void onPassKeyNotify(uint32_t pass_key) {
+    Serial.println("On passkey Notify number: " + String(pass_key));
+  }
+  bool onConfirmPIN(uint32_t pass_key) {
+    Serial.println("onConfirmPIN");
+    return true;
+  }
+  bool onSecurityRequest() {
+    Serial.println("onSecurityRequest");
+    return true;
+  }
+  void onAuthenticationComplete(esp_ble_auth_cmpl_t cmpl) {
+    if(cmpl.success){
+      Serial.println("Auth Complete");
+      authenticated = true;
+      esp_ble_gap_update_whitelist(ESP_BLE_WHITELIST_ADD, cmpl.bd_addr, BLE_WL_ADDR_TYPE_PUBLIC);
+      preferences.begin("ble_whitelist", false);
+      preferences.putBytes("peer_addr", cmpl.bd_addr, sizeof(cmpl.bd_addr));
+      preferences.end();
+    } else {
+      Serial.println("Auth Failed");
+      authenticated = false;
+    }
   }
 };
 
-class SecurityCallback : public BLESecurityCallbacks
-{
-
-  uint32_t onPassKeyRequest()
-  {
-    return 000000;
-  }
-
-  void onPassKeyNotify(uint32_t pass_key) {}
-
-  bool onConfirmPIN(uint32_t pass_key)
-  {
-    vTaskDelay(5000);
-    return true;
-  }
-
-  bool onSecurityRequest()
-  {
-    return true;
-  }
-
-  void onAuthenticationComplete(esp_ble_auth_cmpl_t cmpl)
-  {
-    if (cmpl.success)
-    {
-      Serial.println("   - SecurityCallback - Authentication Success");
-      ConStatus = true;
+void notifyTask(void * parameter) {
+  while (true) {
+    if (deviceConnected && authenticated) {
+      pCharacteristic->setValue(&txValue, 1);
+      pCharacteristic->notify();
+      txValue++;
+      if (txValue > 255) txValue = 0;
+      digitalWrite(ledPin, HIGH);
+      vTaskDelay(pdMS_TO_TICKS(500));
+      digitalWrite(ledPin, LOW);
+      vTaskDelay(pdMS_TO_TICKS(500));
+    } else {
+      digitalWrite(ledPin, LOW);
+      vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    else
-    {
-      Serial.println("   - SecurityCallback - Authentication Failure*");
-      pServer->removePeerDevice(pServer->getConnId(), true);
-    }
-    BLEDevice::startAdvertising();
   }
-};
-
-void bleSecurity()
-{
-  esp_ble_auth_req_t auth_req = ESP_LE_AUTH_REQ_SC_MITM_BOND;
-  esp_ble_io_cap_t iocap = ESP_IO_CAP_OUT;
-  uint8_t key_size = 16;
-  uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
-  uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
-  uint32_t passkey = PASSKEY;
-  uint8_t auth_option = ESP_BLE_ONLY_ACCEPT_SPECIFIED_AUTH_DISABLE;
-  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_STATIC_PASSKEY, &passkey, sizeof(uint32_t));
-  esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(uint8_t));
-  esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap, sizeof(uint8_t));
-  esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE, &key_size, sizeof(uint8_t));
-  esp_ble_gap_set_security_param(ESP_BLE_SM_ONLY_ACCEPT_SPECIFIED_SEC_AUTH, &auth_option, sizeof(uint8_t));
-  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key, sizeof(uint8_t));
-  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t));
 }
 
-void bleInit()
-{
-  BLEDevice::init("BLE-Secure-Server");
-  BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT);
-  BLEDevice::setSecurityCallbacks(new SecurityCallback());
+void reconnectTask(void * parameter) {
+  while (true) {
+    if (!deviceConnected) {
+      // Начинаем рекламировать устройство для попытки переподключения
+      pServer->getAdvertising()->start();
+      vTaskDelay(pdMS_TO_TICKS(5000));
+    } else {
+      vTaskDelay(pdMS_TO_TICKS(10000));
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  BLEDevice::init(DEVICE_NAME);
 
   pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new ServerCallback());
+  pServer->setCallbacks(new MyServerCallbacks());
 
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_NOTIFY);
 
-  pCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  BLESecurity *pSecurity = new BLESecurity();
+  pSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_MITM_BOND);
+  pSecurity->setCapability(ESP_IO_CAP_OUT);
+  pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+  pSecurity->setKeySize(16);
+  pSecurity->setStaticPIN(123456);
+  BLEDevice::setSecurityCallbacks(new MySecurity());
+
   pService->start();
 
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);
-  BLEDevice::startAdvertising();
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMaxPreferred(0x12);
 
-  bleSecurity();
-}
+  esp_ble_gap_config_local_privacy(true);
 
-static void blink_task(void *arg)
-{
-  while (1)
-  {
-    if(ConStatus)
-    {
-      digitalWrite(22, LOW);
-      vTaskDelay(pdMS_TO_TICKS(500));
-      digitalWrite(22, HIGH);
+  // Считываем адрес из памяти и добавляем в белый список
+  preferences.begin("ble_whitelist", true);
+  uint8_t peer_addr[6];
+  if (preferences.getBytes("peer_addr", peer_addr, sizeof(peer_addr)) == sizeof(peer_addr)) {
+    esp_ble_gap_update_whitelist(ESP_BLE_WHITELIST_ADD, peer_addr, BLE_WL_ADDR_TYPE_PUBLIC);
+    Serial.print("Restored peer address from whitelist: ");
+    for (int i = 0; i < 6; i++) {
+      if (i != 0) {
+        Serial.print(":");
+      }
+      Serial.print(peer_addr[i], HEX);
     }
-    vTaskDelay(pdMS_TO_TICKS(500));
+    Serial.println();
+  } else {
+    Serial.println("No peer address found in whitelist");
   }
+  preferences.end();
+
+  pAdvertising->start();
+  Serial.println("Waiting for a client connection to notify...");
+
+  xTaskCreate(notifyTask, "notifyTask", 4096, NULL, 1, NULL);
+  xTaskCreate(reconnectTask, "reconnectTask", 4096, NULL, 1, NULL);
 }
 
-static void ble_task(void *arg)
-{
-  uint32_t value = 0;
-
-  while (1)
-  {
-
-    xSemaphoreTake(xGuiSemaphore, portMAX_DELAY);
-    int connectedCount = pServer->getConnectedCount();
-    Serial.printf("connectedCount: %d\n", connectedCount);
-
-    if (connectedCount > 0)
-    {
-      pCharacteristic->setValue((uint8_t *)&value, 4);
-      pCharacteristic->notify();
-    }
-    value++;
-
-    xSemaphoreGive(xGuiSemaphore);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-  }
+void loop() {
+  // Ничего не делаем в loop, вся работа выполняется в задачах
 }
-
-/////////////////////
-//  BLE Code END   //
-/////////////////////
-
-void pinoutInit()
-{
-  pinMode(22, OUTPUT);
-  digitalWrite(22, HIGH);
-}
-
-void setup()
-{
-  Serial.begin(115200);
-  pinoutInit();
-  bleInit();
-  xGuiSemaphore = xSemaphoreCreateMutex();
-  xTaskCreate(blink_task, "blink", 2048, NULL, 1, NULL);
-  delay(500);
-  xTaskCreate(ble_task, "ble_task", 2048, NULL, 2, NULL);
-}
-
-void loop() {}
